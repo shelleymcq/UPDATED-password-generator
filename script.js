9b202731400f8a730f7a030d6f60cd4e7ee6ad2b
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-10-30 07:43:16 -0400

    added simple validation to user choices prompts

--- script.js
+++ script.js
@@ -23,22 +23,58 @@ function getInput() {
 
     let choices = {};
 
+    let passwordLength = parseInt(prompt("Enter a password length. Password must be between 8 - 100 characters."));
+        if (passwordLength) {
+           choices.passwordLength = passwordLength;
+        } 
+        if(Number.isNaN(passwordLength)){
+            alert("that is not a number");
+            return;
+          }
+          if (passwordLength < 8 ) {
+            alert("you must have a minimum length of 8");
+            return;
+          }
+          if (passwordLength > 100 ){
+            alert("you can have a maximum length of 100");
+            return;
+          }
+
     let lowerCase = confirm("Would you like to include lowercase letters? Ex: a, b, c, ...");
         if (lowerCase) {
-            choices.lowerCase = "true";
-        } 
+            choices.lowerCase = true;
+        } else {
+            choices.lowerCase = false;
+        }
     let upperCase = confirm("Would you like to include uppercase letters? Ex: A, B, C, ...");
         if (upperCase) {
-            choices.upperCase = "true";
-        } 
+            choices.upperCase = true;
+        } else {
+            choices.upperCase = false;
+        }
     let numbers = confirm("Would you like to include numbers? Ex: 1, 2, 3, ...");
         if (numbers) {
-            choices.numbers = "true";
-        } 
+            choices.numbers = true;
+        } else {
+            choices.numbers = false;
+        }
     let symbols = confirm("Would you like to include symbols? Ex: #, $, *, ...");
         if (symbols) {
-            choices.symbols = "true";
-        } 
+            choices.symbols = true;
+        } else {
+            choices.symbols = false;
+        }
+
+    if (
+        lowerCase === false && 
+        upperCase === false &&
+        numbers === false &&
+        symbols === false
+    ) {
+        alert("You must choose at least one type of character to include in password.");
+        return;
+    }
+
     console.log(choices);
     return choices;
 
@@ -49,17 +85,3 @@ function getInput() {
 // add event listener to generate button
 generateBtn.addEventListener("click", getInput);
 
-
-// switch(favDrink) {
-//     case "Martini":
-//       text = "Excellent choice! Martini is good for your soul.";
-//       break;
-//     case "Daiquiri":
-//       text = "Daiquiri is my favorite too!";
-//       break;
-//     case "Cosmopolitan":
-//       text = "Really? Are you sure the Cosmopolitan is your favorite?";
-//       break;
-//     default:
-//       text = "I have never heard of that one..";
-//       break;
